ea2c298943bd1acca66194a4f385dc314c9cb94d
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-05-03 18:55:17 -0300

    fix broken migration.

--- src/migrations/20210501220812_add_not_null_to_ratings_score.js
+++ src/migrations/20210501220812_add_not_null_to_ratings_score.js
@@ -1,9 +1,7 @@
 export const up = (knex) => knex.schema.table('ratings', (t) => {
-  t.timestamp('created_at').defaultTo(knex.fn.now());
-  t.integer('score').notNull();
+  t.integer('score').notNull().alter();
 });
 
 export const down = (knex) => knex.schema.table('ratings', (t) => {
-  t.dropColumn('created_at');
   t.dropColumn('score');
 });
