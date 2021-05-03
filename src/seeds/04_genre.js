6ca5f26ce7940609eeb470f398b71eba25d7cb19
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-05-03 18:57:34 -0300

    add queries to reset sequences when inserting data through seeds.

--- src/seeds/04_genre.js
+++ src/seeds/04_genre.js
@@ -1,5 +1,7 @@
 import genre from './data/genre.js';
 
-export const seed = (knex) => knex('genre').del().then(() => knex('genre').insert(genre));
+export const seed = (knex) => knex('genre').del()
+  .then(() => knex('genre').insert(genre))
+  .then(() => knex.raw('ALTER SEQUENCE genre_genre_id_seq RESTART WITH 4'));
 
 export default seed;
--- src/seeds/06_ratings.js
+++ src/seeds/06_ratings.js
@@ -1,5 +1,7 @@
 import ratings from './data/ratings.js';
 
-export const seed = (knex) => knex('ratings').del().then(() => knex('ratings').insert(ratings));
+export const seed = (knex) => knex('ratings').del()
+  .then(() => knex('ratings').insert(ratings))
+  .then(() => knex.raw('ALTER SEQUENCE ratings_rating_id_seq RESTART WITH 3'));
 
 export default seed;
