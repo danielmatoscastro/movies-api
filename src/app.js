ab647eba2a7e8079e4f95e662caa8ccd67fdcc68
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-05-03 15:57:10 -0300

    add endpoint to list all ratings.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './routes/moviesRoutes.js';
+import ratingsRouter from './routes/ratingsRoutes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config({
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.get('/', (req, res) => res.json({ message: 'ok' }));
 app.use('/', moviesRouter);
+app.use('/', ratingsRouter);
 
 app.use(errorHandler);
 
---
+++ src/tests/ratings.test.js
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import app from '../app.js';
+import knex from '../connection.js';
+import movies from '../seeds/data/movies.js';
+import ratings from '../seeds/data/ratings.js';
+
+describe('ratings-related endpoints', () => {
+  beforeAll(async () => {
+    await knex('ratings').del();
+    await knex('movies').insert(movies).onConflict('movie_id').ignore();
+    return knex('ratings').insert(ratings);
+  });
+
+  describe('GET /ratings', () => {
+    it('should list all ratings', async () => {
+      const response = await request(app).get('/ratings');
+
+      expect(response.status).toBe(200);
+      expect(response.body.length).toEqual(ratings.length);
+    });
+  });
+
+  describe('POST /ratings', () => {
+    it('should list all ratings', async () => {
+      const response = await request(app).get('/ratings');
+
+      expect(response.status).toBe(200);
+      expect(response.body.length).toEqual(ratings.length);
+    });
+  });
+});
